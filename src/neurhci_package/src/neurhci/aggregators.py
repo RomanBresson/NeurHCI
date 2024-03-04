f6c5f596b3a09b500ce90d810a3b2b7bd79f9f96
RomanBresson <roman.bresson.5@gmail.com>
2024-03-04 21:45:59 +0100

    Added general shapley value for 0-1 based CIs

--- src/neurhci_package/src/neurhci/aggregators.py
+++ src/neurhci_package/src/neurhci/aggregators.py
@@ -5,7 +5,55 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class CI2Add(nn.Module):
+class Aggregator(nn.Module):
+    def __init__(self, dimension):
+        super(Aggregator, self).__init__()
+        self.dim = dimension
+        self.output = 0.
+        self.input = torch.zeros(self.dim)
+
+    def shapley_value_single_node(self, x, y, i):
+        """
+            x: tensor of shape b x self.dim
+            y: tensor of shape b x self.dim
+            i: integer, index of child to explain
+
+            Computes the Shapley values for the ith input feature for explaining the difference between two inputs.
+        """
+        with torch.no_grad():
+            shap = torch.zeros(x.shape[0], 1)
+            divisor = 0.
+            for pos_in_permutation in range(self.dim):
+                #We do not explicitely use all permutations, sice many are equivalent for our purpose.
+                #Thus, we only consider splits, and reajust by the number of underlying permutations represented by each split.
+                other_children = [c for c in range(self.dim) if c!=i]
+                all_splits = itertools.combinations(other_children, pos_in_permutation)
+                for split_children in all_splits:
+                    after_explained = list(split_children) #these  elements will be y
+                    nb_permutations = factorial(pos_in_permutation)*factorial(self.dim-pos_in_permutation-1)
+                    first_half = x*1
+                    first_half[:, after_explained] = y[:, after_explained]
+                    second_half = first_half*1
+                    second_half[:, i] = y[:, i]
+                    compounds_inputs = torch.cat([first_half, second_half])
+                    compounds_outputs = self.forward(compounds_inputs)
+                    deltas = compounds_outputs[x.shape[0]:]-compounds_outputs[:x.shape[0]]
+                    shap += deltas*nb_permutations
+                    divisor += nb_permutations
+            shap /= divisor
+        return(shap)
+
+    def shapley_values(self, x, y):
+        """
+            x: tensor of shape b x self.dim
+            y: tensor of shape b x self.dim
+
+            Computes the Shapley values for input feature for explaining the difference between two inputs.
+        """
+        shap = torch.cat([self.local_shapley_value(x, y, i) for i in range(self.dim)], dim=1)
+        return(shap)
+
+class CI2Add(Aggregator):
     """
         A module representing a 2-additive Choquet integral.
 
@@ -16,13 +64,10 @@ class CI2Add(nn.Module):
         """
             dimension: the number of aggregated features
         """
-        super(CI2Add, self).__init__()
-        self.dim = dimension
+        super(CI2Add, self).__init__(dimension)
         self.preweight = nn.Parameter(torch.randn((1,self.dim**2)))
         self.update_weight()
-        self.output = 0.
-        self.input = torch.zeros(self.dim)
-    
+
     def update_weight(self):
         self.weight = F.softmax(self.preweight, dim=1)
 
@@ -104,94 +149,48 @@ class CI2Add(nn.Module):
                     current_node += 1
         return(shapley)
 
-    def shapley_value_single_node(self, x, y, i):
-        """
-            x: tensor of shape b x self.dim
-            y: tensor of shape b x self.dim
-            i: integer, index of child to explain
-
-            Computes the Shapley values for the ith input feature for explaining the difference between two inputs.
-        """
-        with torch.no_grad():
-            shap = torch.zeros(x.shape[0], 1)
-            divisor = 0.
-            for pos_in_permutation in range(self.dim):
-                #We do not explicitely use all permutations, sice many are equivalent for our purpose.
-                #Thus, we only consider splits, and reajust by the number of underlying permutations represented by each split.
-                other_children = [c for c in range(self.dim) if c!=i]
-                all_splits = itertools.combinations(other_children, pos_in_permutation)
-                for split_children in all_splits:
-                    after_explained = list(split_children) #these  elements will be y
-                    nb_permutations = factorial(pos_in_permutation)*factorial(self.dim-pos_in_permutation-1)
-                    first_half = x*1
-                    first_half[:, after_explained] = y[:, after_explained]
-                    second_half = first_half*1
-                    second_half[:, i] = y[:, i]
-                    compounds_inputs = torch.cat([first_half, second_half])
-                    compounds_outputs = self.forward(compounds_inputs)
-                    deltas = compounds_outputs[x.shape[0]:]-compounds_outputs[:x.shape[0]]
-                    shap += deltas*nb_permutations
-                    divisor += nb_permutations
-            shap /= divisor
-        return(shap)
-
-    def shapley_values(self, x, y):
-        """
-            x: tensor of shape b x self.dim
-            y: tensor of shape b x self.dim
-
-            Computes the Shapley values for input feature for explaining the difference between two inputs.
-        """
-        shap = torch.cat([self.local_shapley_value(x, y, i) for i in range(self.dim)], dim=1)
-        return(shap)
-
-class CI01FromAntichain(nn.Module):
+class CI01FromAntichain(Aggregator):
     """
         A Choquet Integral with a 0-1 Fuzzy Measure defined as its associated antichain.
 
         An antichain is a tuple of tuples of indices. The associated CI is provably a max(min)
     """
     def __init__(self, antichain, dimension):
-        super(CI01FromAntichain, self).__init__()
-        self.dim = dimension
-        self.antichain = [torch.tensor(c) for c in antichain]
-        self.set_shapley_values_global() #can be computed easily.
+        super(CI01FromAntichain, self).__init__(dimension)
+
+        #remove duplicate and inclusion relations
+        no_duplicate = list(set(antichain))
+        simple_antichain = [tuple_a for i, tuple_a in enumerate(no_duplicate)
+                                if not any(set(tuple_a).issuperset(set(tuple_b))
+                                for j, tuple_b in enumerate(no_duplicate)
+                                if i != j)]
+        self.antichain = simple_antichain
+        self.antichain_tensors = [torch.tensor(c) for c in self.antichain]
+        self.set_shapley_values_global() #can be computed only once since no learnable parameter
         self.output = 0.
 
     def set_shapley_values_global(self):
-        # can probably be made more general
+        # can probably be made more optima
         # works ONLY for the pre-designed patterns from CI3addFrom01
-        shape_ac = [len(a) for a in self.antichain]
+        relevant_indices = set(itertools.chain.from_iterable(self.antichain))
         shaps = torch.zeros(self.dim)
-        if len(shape_ac)==1:
-            shaps[self.antichain[0]] = 1.
-        elif (shape_ac==[1,1]) or (shape_ac==[1,1,1]):
-            shaps[torch.cat(self.antichain)] = 1.
-        elif (shape_ac==[1,2]):
-            shaps[self.antichain[0]] = 2.
-            shaps[self.antichain[1]] = 0.5
-        elif (shape_ac==[2,2]):
-            shaps[self.antichain[0]] += 0.5
-            shaps[self.antichain[1]] += 0.5
-            shaps = torch.where(shaps>0.75, 2., shaps)
-        else:
-            raise ValueError("Invalid antichain, may be handled in a later version")
-        self.global_shapley_values = (shaps/sum(shaps))
+        for r in relevant_indices:
+            shaps[r] = self.shapley_value_single_node(torch.zeros(1, self.dim), torch.ones(1, self.dim), r).item()
+        self.global_shapley_values = shaps
 
     def forward(self, x):
         self.input = x
-        get_mins = torch.cat([torch.min(x[:,group], dim=1).values.unsqueeze(1) for group in self.antichain], dim=1)
+        get_mins = torch.cat([torch.min(x[:,group], dim=1).values.unsqueeze(1) for group in self.antichain_tensors], dim=1)
         self.output = torch.max(get_mins, dim=1).values.unsqueeze(1)
         return(self.output)
 
     def shapley_values_global(self):
         return(self.global_shapley_values)
 
-class CI3addFrom01(nn.Module):
+class CI3addFrom01(Aggregator):
     # could be optimized through tensor manipulation
     def __init__(self, dimension):
-        super(CI3addFrom01, self).__init__()
-        self.dim = dimension
+        super(CI3addFrom01, self).__init__(dimension)
         all_doubles = itertools.combinations(range(self.dim), 2)
         all_triples = itertools.combinations(range(self.dim), 3)
         self.zero_one_CIs = torch.nn.ModuleList({})
